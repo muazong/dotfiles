0d8dc150acb8edf26bb4d69ceafd721576525139
aZong <giongmua2004@gmail.com>
2025-02-05 18:31:43 +0700

    remove eslint config

--- nvim/.config/nvim/lua/azong/plugins/lsp/lspconfig.lua
+++ nvim/.config/nvim/lua/azong/plugins/lsp/lspconfig.lua
@@ -35,18 +35,6 @@ return {
         })
       end,
 
-      ["eslint"] = function()
-        lspconfig["eslint"].setup({
-          capabilities = capabilities,
-          on_attach = function(client, bufnr)
-            vim.api.nvim_create_autocmd("BufWritePre", {
-              buffer = bufnr,
-              command = "EslintFixAll",
-            })
-          end,
-        })
-      end,
-
       -- VueJs
       ["volar"] = function()
         lspconfig["volar"].setup({
