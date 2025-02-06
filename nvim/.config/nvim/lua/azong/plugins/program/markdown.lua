095862b0799f8902578f0018b4b4ddd9a9ec2fb5
aZong <giongmua2004@gmail.com>
2025-02-06 23:22:25 +0700

    update

--- nvim/.config/nvim/lua/azong/plugins/program/markdown.lua
+++ nvim/.config/nvim/lua/azong/plugins/program/markdown.lua
@@ -1,5 +1,6 @@
 return {
   "iamcco/markdown-preview.nvim",
+  ft = { "markdown" },
   cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
   build = "cd app && yarn install",
   init = function()
