1fecc3bd47f7571c5bcc8fb616b600b78002cb46
aZong <giongmua2004@gmail.com>
2025-02-12 10:14:50 +0700

    add convert plugin

---
+++ nvim/.config/nvim/lua/azong/plugins/utils/convert.lua
@@ -0,0 +1,19 @@
+return {
+  "cjodo/convert.nvim",
+  dependencies = {
+    "MunifTanjim/nui.nvim",
+  },
+  keys = { { "<leader>cc", "<cmd>ConvertFindCurrent<CR>" } },
+  config = function()
+    local convert = require("convert")
+    convert.setup({
+      keymaps = {
+        focus_next = { "j", "<Down>", "<Tab>" },
+        focus_prev = { "k", "<Up>", "<S-Tab>" },
+        close = { "<Esc>", "<C-c>", "q" },
+        submit = { "<CR>", "<Space>" },
+      },
+      modes = { "color", "size", "numbers" },
+    })
+  end,
+}
