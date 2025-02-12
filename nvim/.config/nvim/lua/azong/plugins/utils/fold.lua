281ae15285f4d22fa919722ab199ab7a91f09198
aZong <giongmua2004@gmail.com>
2025-02-12 10:14:56 +0700

    add fold plugin

---
+++ nvim/.config/nvim/lua/azong/plugins/utils/fold.lua
@@ -0,0 +1,23 @@
+return {
+  "kevinhwang91/nvim-ufo",
+  event = { "BufReadPost", "BufNewFile" },
+  dependencies = { "kevinhwang91/promise-async" },
+  config = function()
+    local opts = vim.opt
+    local maps = vim.keymap.set
+
+    opts.foldcolumn = "1"
+    opts.foldlevel = 99
+    opts.foldlevelstart = 99
+    opts.foldenable = true
+
+    maps("n", "zA", require("ufo").openAllFolds)
+    maps("n", "zM", require("ufo").closeAllFolds)
+
+    require("ufo").setup({
+      provider_selector = function()
+        return { "treesitter", "indent" }
+      end,
+    })
+  end,
+}
