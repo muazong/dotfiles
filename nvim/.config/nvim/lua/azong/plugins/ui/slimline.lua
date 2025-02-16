d63105a74dcd66436241884e5445e5c295136b1c
aZong <giongmua2004@gmail.com>
2025-02-16 15:38:06 +0700

    update

--- nvim/.config/nvim/lua/azong/plugins/ui/slimline.lua
+++ nvim/.config/nvim/lua/azong/plugins/ui/slimline.lua
@@ -3,22 +3,21 @@ return {
   event = "VimEnter",
   config = function()
     require("slimline").setup({
-      bold = true, -- makes primary parts and mode bold
-      verbose_mode = false, -- Mode as single letter or as a word
-      style = "fg", -- or "fg". Whether highlights should be applied to bg or fg of components
-      mode_follow_style = true, -- Whether the mode color components should follow the style option
-      workspace_diagnostics = false, -- Whether diagnostics should show workspace diagnostics instead of current buffer
+      bold = true,
+      verbose_mode = false,
+      style = "fg", -- or "bg"
+      mode_follow_style = true,
+      workspace_diagnostics = true,
       components = {
         left = {
           "mode",
-          -- "path",
           "git",
         },
         center = {
           "path",
         },
         right = {
-          -- "diagnostics",
+          "diagnostics",
           -- "filetype_lsp",
           "progress",
         },
@@ -30,11 +29,11 @@ return {
       },
       sep = {
         hide = {
-          first = false, -- hides the first separator
-          last = false, -- hides the last separator
+          first = true,
+          last = true,
         },
-        left = " ",
-        right = " ",
+        left = "",
+        right = "",
       },
       hl = {
         modes = {
