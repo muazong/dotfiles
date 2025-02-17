9b4bf9eb266f807e03f19b9e90d5d9ddb1bf38c9
aZong <giongmua2004@gmail.com>
2025-02-18 01:18:35 +0700

    update

--- scripts/.config/scripts/checking-weather.sh
+++ scripts/.config/scripts/checking-weather.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
-if ping -c 3 8.8.8.8 &>/dev/null; then
+process_weather() {
+  echo "$1" | tr -d '\r\n' | sed 's/[[:space:]]*$//'
+}
+
+if ping -c 3 -W 1 8.8.8.8 &>/dev/null; then
   location="Sontay"
-  weather=$(curl -s "wttr.in/$location?format=%t+%c")
+  weather_raw=$(curl -s "wttr.in/$location?format=%t+%c")
 
-  if [[ -n "$weather" ]]; then
-    weather=$(echo "$weather" | tr -d '\r')
-    weather=$(echo "$weather" | tr -d '\n')
-
-    echo "$location, $weather" | sed 's/[[:space:]]*$//' | tr -d '+'
+  if [[ -n "$weather_raw" ]]; then
+    weather=$(process_weather "$weather_raw")
+    printf "%s, %s\n" "$location" "$weather" | tr -d '+'
   else
     echo "No Information."
   fi
 else
-  echo "No Network."
+  echo "No Internet."
 fi
