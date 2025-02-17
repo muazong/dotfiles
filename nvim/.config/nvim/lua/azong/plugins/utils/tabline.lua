7b78baf90372da963d6f3d71feeea0b41a9e76a7
aZong <giongmua2004@gmail.com>
2025-02-17 22:13:16 +0700

    replace tabline with bufferline

---
+++ nvim/.config/nvim/lua/azong/plugins/utils/tabline.lua
@@ -0,0 +1,55 @@
+return {
+  "rafcamlet/tabline-framework.nvim",
+  config = function()
+    require("tabline_framework").setup({
+      render = function(tab)
+        tab.add(" ")
+        tab.make_tabs(function(info)
+          if info.filename and info.modified then
+            tab.add(" ")
+            tab.add({ info.index, fg = "#FF9E3B", gui = "bold,underline,italic" })
+            tab.add({ " ", fg = "#FF9E3B", gui = "bold,underline,italic" })
+
+            tab.add({ info.filename, fg = "#FF9E3B", gui = "bold,underline,italic" })
+
+            tab.add({
+              " " .. tab.icon(info.filename),
+              fg = tab.icon_color(info.filename),
+            })
+
+            tab.add({ " ", fg = "#FF9E3B", gui = "bold" })
+
+            tab.add(" ")
+          elseif info.filename then
+            tab.add(" " .. info.index .. " ")
+            tab.add(info.filename)
+            tab.add({
+              " " .. tab.icon(info.filename),
+              fg = tab.icon_color(info.filename),
+            })
+            tab.add(" ")
+          else
+            tab.add(" " .. info.index .. " [No name] ")
+          end
+        end)
+
+        tab.add_spacer()
+
+        local errors = #vim.diagnostic.get(nil, { severity = vim.diagnostic.severity.ERROR })
+        local warnings = #vim.diagnostic.get(nil, { severity = vim.diagnostic.severity.WARN })
+        local info = #vim.diagnostic.get(nil, { severity = vim.diagnostic.severity.INFO })
+        local hint = #vim.diagnostic.get(nil, { severity = vim.diagnostic.severity.HINT })
+
+        tab.add({ "  " .. errors, fg = "#FF5D62" })
+        tab.add({ "  " .. warnings, fg = "#E6C384" })
+        tab.add({ "  " .. info, fg = "#A3D4D5" })
+        tab.add({ " 󱩎 " .. hint, fg = "#7E9CD8" })
+
+        tab.add(" ")
+      end,
+      hl = { fg = "#727169", bg = "#1F1F28" },
+      hl_sel = { fg = "#FEF9D9", bg = "#1F1F28", gui = "italic" },
+      hl_fill = { fg = "#727169", bg = "#1F1F28" },
+    })
+  end,
+}
