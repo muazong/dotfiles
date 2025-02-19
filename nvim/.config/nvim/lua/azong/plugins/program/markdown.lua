6baec512718db85b103821ca9b72a07f8d2cdc9d
aZong <giongmua2004@gmail.com>
2025-02-19 10:43:47 +0700

    add more plugin tool for markdown

--- nvim/.config/nvim/lua/azong/plugins/program/markdown.lua
+++ nvim/.config/nvim/lua/azong/plugins/program/markdown.lua
@@ -1,10 +1,23 @@
 return {
-  "iamcco/markdown-preview.nvim",
-  ft = { "markdown" },
-  cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-  build = "cd app && yarn install",
-  init = function()
-    vim.g.mkdp_filetypes = { "markdown" }
-    vim.g.mkdp_port = "5000"
-  end,
+  { -- Markdown preview use local server
+    "iamcco/markdown-preview.nvim",
+    ft = { "markdown" },
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    build = "cd app && yarn install",
+    init = function()
+      vim.g.mkdp_filetypes = { "markdown" }
+      vim.g.mkdp_port = "5000"
+    end,
+  },
+  { -- Markdown preview inside neovim
+    "OXY2DEV/markview.nvim",
+    ft = { "markdown", "md" },
+    config = function()
+      require("markview").setup({
+        preview = {
+          icon_provider = "devicons",
+        },
+      })
+    end,
+  },
 }
