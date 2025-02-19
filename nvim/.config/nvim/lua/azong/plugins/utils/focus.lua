799dde1972cf7f1edcdb34d0fe5298fd6ac71d4f
aZong <giongmua2004@gmail.com>
2025-02-19 10:21:23 +0700

    replace focus.lua with windows.lua

---
+++ nvim/.config/nvim/lua/azong/plugins/utils/focus.lua
@@ -0,0 +1,27 @@
+return {
+  "nvim-focus/focus.nvim",
+  keys = {
+    { "<leader>ss", "<cmd>split<cr>" },
+    { "<leader>sv", "<cmd>vsplit<cr>" },
+  },
+  config = function()
+    vim.api.nvim_create_autocmd("WinEnter", {
+      group = vim.api.nvim_create_augroup("FocusDisable", { clear = true }),
+      callback = function(_)
+        if vim.tbl_contains({ "nofile", "prompt", "popup" }, vim.bo.buftype) then
+          vim.w.focus_disable = true
+        else
+          vim.w.focus_disable = false
+        end
+      end,
+    })
+
+    require("focus").setup({
+      commands = false,
+      ui = {
+        cursorline = false,
+        signcolumn = false,
+      },
+    })
+  end,
+}
