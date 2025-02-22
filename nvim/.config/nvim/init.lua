dd4efecd40d6226600a00c2c06a3de3ac7090983
aZong <giongmua2004@gmail.com>
2025-02-22 23:40:13 +0700

    add debug util for neovim

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -2,5 +2,11 @@ if vim.loader then
   vim.loader.enable()
 end
 
+_G.dd = function(...)
+  require("azong.util.debug").dump(...)
+end
+vim.print = _G.dd
+
 require("azong.core")
+require("azong.util")
 require("azong.lazy")
