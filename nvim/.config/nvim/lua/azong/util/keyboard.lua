7b54734aa617d421ae736516e284cc25eb70b256
aZong <giongmua2004@gmail.com>
2025-02-23 00:10:37 +0700

    update

--- nvim/.config/nvim/lua/azong/util/keyboard.lua
+++ nvim/.config/nvim/lua/azong/util/keyboard.lua
@@ -1,29 +1,29 @@
 local M = {}
 
-local KEYBOARD_TYPES = {
+M.KEYBOARD_TYPES = {
   default = "Default",
   telex = "Vietnamese Telex",
 }
 
-local message = KEYBOARD_TYPES.default
+M.message = M.KEYBOARD_TYPES.default
 local telex_enabled = false
 
-local toggle_telex = function()
+M.toggle_telex = function()
   telex_enabled = not telex_enabled
 
   if telex_enabled then
     vim.o.keymap = "vietnamese-telex_utf-8"
-    message = KEYBOARD_TYPES.telex
+    M.message = M.KEYBOARD_TYPES.telex
   else
     vim.o.keymap = ""
-    message = KEYBOARD_TYPES.default
+    M.message = M.KEYBOARD_TYPES.default
   end
 
-  vim.notify(message, vim.log.levels.INFO, {
+  vim.notify(M.message, vim.log.levels.INFO, {
     title = "Keyboard Layout",
   })
 end
 
-vim.keymap.set("n", "<Leader>v", toggle_telex, { noremap = true, silent = true })
+vim.keymap.set("n", "<Leader>v", M.toggle_telex, { noremap = true, silent = true })
 
 return M
