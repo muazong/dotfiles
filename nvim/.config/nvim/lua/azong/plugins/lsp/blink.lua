baea8f73ac953e7ff644dc6623846784b9ec6976
aZong <giongmua2004@gmail.com>
2025-02-26 01:42:14 +0700

    update and fix blink cmp slow

--- nvim/.config/nvim/lua/azong/plugins/lsp/blink.lua
+++ nvim/.config/nvim/lua/azong/plugins/lsp/blink.lua
@@ -2,6 +2,7 @@ return {
   "saghen/blink.cmp",
   event = "InsertEnter",
   version = "*",
+  build = "cargo build --release",
   opts = {
     enabled = function()
       return not vim.tbl_contains({}, vim.bo.filetype)
@@ -10,107 +11,45 @@ return {
         and vim.b.completion ~= false
     end,
     sources = {
-      default = {
-        "lsp",
-        "snippets",
-        "path",
-        "buffer",
-        -- "codeium",
-        -- "copilot",
-      },
-      providers = {
-        lsp = {
-          name = "lsp",
-          enabled = true,
-          module = "blink.cmp.sources.lsp",
-          score_offset = 100,
-          max_items = 5,
-        },
-        snippets = {
-          name = "snippets",
-          enabled = true,
-          module = "blink.cmp.sources.snippets",
-          score_offset = 90,
-          max_items = 5,
-        },
-        path = {
-          name = "Path",
-          module = "blink.cmp.sources.path",
-          score_offset = 25,
-          fallbacks = { "snippets", "buffer", "lsp" },
-          opts = {
-            trailing_slash = false,
-            label_trailing_slash = true,
-            get_cwd = function(context)
-              return vim.fn.expand(("#%d:p:h"):format(context.bufnr))
-            end,
-            show_hidden_files_by_default = true,
-          },
-        },
-        buffer = {
-          name = "Buffer",
-          enabled = true,
-          max_items = 3,
-          module = "blink.cmp.sources.buffer",
-          score_offset = 15,
-        },
-        --[[ copilot = {
-          name = "copilot",
-          module = "blink-copilot",
-          score_offset = 100,
-          async = true,
-        }, ]]
-        --[[ codeium = {
-          name = "codeium",
-          module = "blink.compat.source",
-          score_offset = 10,
-          async = true,
-        }, ]]
-      },
+      default = { "lsp", "path", "buffer" },
     },
     keymap = {
       preset = "none",
       ["<CR>"] = { "select_and_accept", "fallback" },
-      ["<S-CR>"] = { "hide" }, -- `hide` keeps `auto_insert`, `cancel` does not
-      ["<C-Space>"] = { "show" },
+      ["<C-e>"] = { "hide" },
+      ["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
+
       ["<Tab>"] = { "snippet_forward", "select_next", "fallback" },
       ["<S-Tab>"] = { "snippet_backward", "select_prev", "fallback" },
-      ["<C-n>"] = { "select_next" },
-      ["<C-p>"] = { "select_prev" },
-      ["<Down>"] = { "select_next", "fallback" },
+
       ["<Up>"] = { "select_prev", "fallback" },
+      ["<Down>"] = { "select_next", "fallback" },
+      ["<C-p>"] = { "select_prev", "fallback" },
+      ["<C-n>"] = { "select_next", "fallback" },
+
       ["<C-d>"] = { "scroll_documentation_down", "fallback" },
       ["<C-u>"] = { "scroll_documentation_up", "fallback" },
     },
     cmdline = {
       keymap = {
-        ["<CR>"] = { "fallback" }, -- CR = execute (no need for accept since using `auto_insert`)
+        ["<CR>"] = { "fallback" },
         ["<Tab>"] = { "select_next", "show" },
         ["<S-Tab>"] = { "select_prev" },
       },
-      sources = function()
-        local type = vim.fn.getcmdtype()
-        if type == "/" or type == "?" then
-          return { "buffer" }
-        end
-        if type == ":" then
-          return { "cmdline" }
-        end
-        return {}
-      end,
     },
     completion = {
-      trigger = {
-        prefetch_on_insert = false,
-        show_in_snippet = false, -- since we overload `<Tab>` with jumping & selection
-        show_on_trigger_character = false,
-      },
-      list = {
-        cycle = { from_top = false }, -- cycle at bottom, but not at the top
+      trigger = { show_on_trigger_character = false },
+      menu = {
+        auto_show = false,
+        draw = {
+          columns = {
+            { "kind_icon", "label_description", "label", gap = 1 },
+          },
+        },
       },
       documentation = {
         auto_show = true,
-        auto_show_delay_ms = 250,
+        auto_show_delay_ms = 500,
         window = {
           scrollbar = false,
           border = "rounded",
@@ -118,81 +57,12 @@ return {
           max_height = 20,
         },
       },
-      menu = {
-        border = vim.g.borderStyle,
-        draw = {
-          align_to = "none", -- keep in place
-          treesitter = { "lsp" },
-          columns = {
-            { "kind_icon", "label_description", "label", gap = 1 },
-          },
-          components = {
-            label = { width = { max = 35 } },
-            label_description = { width = { max = 20 } },
-            kind_icon = {
-              text = function(ctx)
-                -- detect emmet-ls
-                local source, client = ctx.item.source_id, ctx.item.client_id
-                local lspName = client and vim.lsp.get_client_by_id(client).name
-                if lspName == "emmet_language_server" then
-                  source = "emmet"
-                end
-
-                -- use source-specific icons, and `kind_icon` only for items from LSPs
-                local sourceIcons = {
-                  snippets = "󰩫",
-                  buffer = "󰦨",
-                  emmet = "",
-                  path = "",
-                  cmdline = "󰘳",
-                  codeium = "",
-                }
-                return sourceIcons[source] or ctx.kind_icon
-              end,
-            },
-          },
-        },
-      },
       ghost_text = { enabled = true },
     },
     appearance = {
-      use_nvim_cmp_as_default = false,
-      nerd_font_variant = "normal",
-      kind_icons = {
-        -- different icons of the corresponding source
-        Text = "󰉿", -- `buffer`
-        Snippet = "󰞘", -- `snippets`
-        File = "", -- `path`
-        Copilot = "", -- `copilot`,
-
-        Folder = "󰉋",
-        Method = "󰊕",
-        Function = "󰡱",
-        Constructor = "",
-        Field = "󰇽",
-        Variable = "󰀫",
-        Class = "󰜁",
-        Interface = "",
-        Module = "",
-        Property = "󰜢",
-        Unit = "",
-        Value = "󰎠",
-        Enum = "",
-        Keyword = "󰌋",
-        Color = "󰏘",
-        Reference = "",
-        EnumMember = "",
-        Constant = "󰏿",
-        Struct = "󰙅",
-        Event = "",
-        Operator = "󰆕",
-        TypeParameter = "󰅲",
-      },
+      use_nvim_cmp_as_default = true,
+      nerd_font_variant = "mono",
     },
   },
-  dependencies = {
-    "rafamadriz/friendly-snippets",
-    { "fang2hou/blink-copilot", enabled = false },
-    { "saghen/blink.compat", enabled = false, opts = { enable_events = true } },
-  },
+  opts_extend = { "sources.default" },
 }
