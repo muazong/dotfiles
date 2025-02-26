cd0362d43b12425e6f79fd5c8d3557da3f269814
aZong <giongmua2004@gmail.com>
2025-02-26 22:29:56 +0700

    update

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -2,11 +2,6 @@ if vim.loader then
   vim.loader.enable()
 end
 
-_G.dd = function(...)
-  require("azong.utils.debug").dump(...)
-end
-vim.print = _G.dd
-
-require("azong.core")
 require("azong.utils")
+require("azong.core")
 require("azong.lazy")
--- nvim/.config/nvim/lua/azong/utils/init.lua
+++ nvim/.config/nvim/lua/azong/utils/init.lua
@@ -1,1 +1,6 @@
+_G.dd = function(...)
+  require("azong.utils.debug").dump(...)
+end
+vim.print = _G.dd
+
 require("azong.utils.keyboard")
