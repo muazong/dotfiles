f47dd9f92d066ed6e85820a8fbb9a55213845b59
aZong <giongmua2004@gmail.com>
2025-02-26 22:55:59 +0700

    replace kulala with rest.nvim

--- nvim/.config/nvim/lua/azong/plugins/program/rest.lua
+++ nvim/.config/nvim/lua/azong/plugins/program/rest.lua
@@ -1,45 +1,9 @@
 return {
-  "rest-nvim/rest.nvim",
-  ft = { "http" },
-  dependencies = { { "nvim-lua/plenary.nvim" } },
-  config = function()
-    vim.api.nvim_create_autocmd("FileType", {
-      pattern = { "json" },
-      callback = function()
-        vim.api.nvim_set_option_value("formatprg", "jq", { scope = "local" })
-      end,
-    })
-
-    vim.keymap.set("n", "<leader>rr", "<cmd>Rest run<cr>")
-
-    require("rest-nvim").setup({
-      result_split_horizontal = false,
-      result_split_in_place = false,
-      stay_in_current_window_after_split = false,
-      skip_ssl_verification = false,
-      encode_url = true,
-      highlight = {
-        enabled = true,
-        timeout = 150,
-      },
-      result = {
-        show_url = true,
-        show_curl_command = true,
-        show_http_info = true,
-        show_headers = true,
-        show_statistics = true,
-        formatters = {
-          json = "jq",
-          html = function(body)
-            return vim.fn.system({ "tidy", "-i", "-q", "-" }, body)
-          end,
-        },
-      },
-      jump_to_request = false,
-      env_file = ".env",
-      custom_dynamic_variables = {},
-      yank_dry_run = true,
-      search_back = true,
-    })
-  end,
+  "mistweaverco/kulala.nvim",
+  ft = { "http", "rest" },
+  keys = {
+    { "<leader>rr", '<cmd>lua require("kulala").run()<cr>' },
+    { "<leader>ra", '<cmd>lua require("kulala").run_all()<cr>' },
+    { "<leader>rl", '<cmd>lua require("kulala").replay()<cr>' },
+  },
 }
