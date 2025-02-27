5a565a1aca13f7daca10dab3468defca50a478a9
aZong <giongmua2004@gmail.com>
2025-02-27 22:39:52 +0700

    update kulala config

--- nvim/.config/nvim/lua/azong/plugins/program/kulala.lua
+++ nvim/.config/nvim/lua/azong/plugins/program/kulala.lua
@@ -6,5 +6,39 @@ return {
     { "<leader>ra", '<cmd>lua require("kulala").run_all()<cr>' },
     { "<leader>rl", '<cmd>lua require("kulala").replay()<cr>' },
   },
-  opts = {},
+  opts = {
+    ui = {
+      display_mode = "split", -- float
+      split_direction = "horizontal", -- vertical
+      default_view = "headers_body",
+      show_variable_info_text = "float", -- false
+      default_winbar_panes = { "headers_body", "headers", "body", "verbose" },
+    },
+    kulala_keymaps = {
+      ["Show all"] = {
+        "1",
+        function()
+          require("kulala.ui").show_headers_body()
+        end,
+      },
+      ["Show headers"] = {
+        "2",
+        function()
+          require("kulala.ui").show_headers()
+        end,
+      },
+      ["Show body"] = {
+        "3",
+        function()
+          require("kulala.ui").show_body()
+        end,
+      },
+      ["Show verbose"] = {
+        "4",
+        function()
+          require("kulala.ui").show_verbose()
+        end,
+      },
+    },
+  },
 }
