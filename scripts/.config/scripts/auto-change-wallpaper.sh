3ba1240bcb9ab15a5fb50e525ab04c7ab89e8c71
aZong <giongmua2004@gmail.com>
2025-03-22 12:25:42 +0700

    feat: 🎸 auto change wallpaper script

---
+++ scripts/.config/scripts/auto-change-wallpaper.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+WALLPAPERS=($(ls -1 ~/dotfiles/wallpapers/*))
+INDEX=0
+
+while true; do
+  hyprctl hyprpaper wallpaper "eDP-1,${WALLPAPERS[$INDEX]}"
+  INDEX=$(((INDEX + 1) % ${#WALLPAPERS[@]}))
+
+  sleep 300 # 5 minutes
+done
