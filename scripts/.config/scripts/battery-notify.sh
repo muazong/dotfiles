b0df193bdf7acba2caf3446b90d128b31e954a38
aZong <giongmua2004@gmail.com>
2025-03-29 10:49:40 +0700

    feat: 🎸 check battery full

--- scripts/.config/scripts/battery-notify.sh
+++ scripts/.config/scripts/battery-notify.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 
 PREV_STATUS=""
-BATTERY_PATH="/sys/class/power_supply/BAT0/status"
+BATTERY_STATUS_PATH="/sys/class/power_supply/BAT0/status"
+BATTERY_CAPACITY_PATH="/sys/class/power_supply/BAT0/capacity"
 
 while true; do
-  STATUS=$(<"$BATTERY_PATH")
-  if [ "$STATUS" != "$PREV_STATUS" ]; then
+  STATUS=$(<"$BATTERY_STATUS_PATH")
+  CAPACITY=$(<"$BATTERY_CAPACITY_PATH")
+
+  if [ "$STATUS" != "$PREV_STATUS" ] || [ "$CAPACITY" -eq 100 ]; then
     MESSAGE=""
     case "$STATUS" in
     "Charging")
-      MESSAGE="🔋 Charging: The device is currently charging."
+      if [ "$CAPACITY" -eq 100 ]; then
+        MESSAGE="🔋 Battery Full: Please unplug the charger."
+      else
+        MESSAGE="🔋 Charging: The device is currently charging."
+      fi
       ;;
     "Discharging")
       MESSAGE="⚡ Not Connected: The charger is not plugged in."
@@ -20,5 +27,5 @@ while true; do
     fi
     PREV_STATUS="$STATUS"
   fi
-  sleep 0.1
+  sleep 1
 done
