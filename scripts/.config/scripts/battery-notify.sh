03c8239481474c8c81e033f8b58238049624d99c
aZong <giongmua2004@gmail.com>
2025-03-29 22:04:37 +0700

    fix: 🐛 check battery when it full

--- scripts/.config/scripts/battery-notify.sh
+++ scripts/.config/scripts/battery-notify.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 PREV_STATUS=""
+FULL_NOTIFIED=0
+PREV_CAPACITY=-1
 BATTERY_STATUS_PATH="/sys/class/power_supply/BAT0/status"
 BATTERY_CAPACITY_PATH="/sys/class/power_supply/BAT0/capacity"
 
@@ -8,24 +10,36 @@ while true; do
   STATUS=$(<"$BATTERY_STATUS_PATH")
   CAPACITY=$(<"$BATTERY_CAPACITY_PATH")
 
-  if [ "$STATUS" != "$PREV_STATUS" ] || [ "$CAPACITY" -eq 100 ]; then
+  if [ "$STATUS" != "$PREV_STATUS" ] || ([ "$CAPACITY" -eq 100 ] && [ "$FULL_NOTIFIED" -eq 0 ]); then
     MESSAGE=""
     case "$STATUS" in
     "Charging")
-      if [ "$CAPACITY" -eq 100 ]; then
+      if [ "$CAPACITY" -eq 100 ] && [ "$FULL_NOTIFIED" -eq 0 ]; then
         MESSAGE="🔋 Battery Full: Please unplug the charger."
-      else
+        FULL_NOTIFIED=1
+      elif [ "$PREV_STATUS" != "Charging" ]; then
         MESSAGE="🔋 Charging: The device is currently charging."
       fi
       ;;
     "Discharging")
-      MESSAGE="⚡ Not Connected: The charger is not plugged in."
+      if [ "$PREV_STATUS" != "Discharging" ]; then
+        MESSAGE="⚡ Not Connected: The charger is not plugged in."
+      fi
+      FULL_NOTIFIED=0
       ;;
     esac
+
     if [ -n "$MESSAGE" ]; then
       notify-send -u normal -t 3000 "$MESSAGE"
     fi
     PREV_STATUS="$STATUS"
   fi
+
+  if [ "$CAPACITY" -lt 100 ]; then
+    FULL_NOTIFIED=0
+  fi
+
+  PREV_CAPACITY="$CAPACITY"
+
   sleep 1
 done
