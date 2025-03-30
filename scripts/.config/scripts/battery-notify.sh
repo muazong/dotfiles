fd7e3efdaa835de40efafd52117acd3a5d0ee917
aZong <giongmua2004@gmail.com>
2025-03-30 15:08:48 +0700

    feat: 🎸 checking batter low and add sound

--- scripts/.config/scripts/battery-notify.sh
+++ scripts/.config/scripts/battery-notify.sh
@@ -1,45 +1,43 @@
 #!/bin/bash
 
-PREV_STATUS=""
-FULL_NOTIFIED=0
-PREV_CAPACITY=-1
 BATTERY_STATUS_PATH="/sys/class/power_supply/BAT0/status"
 BATTERY_CAPACITY_PATH="/sys/class/power_supply/BAT0/capacity"
+PREV_STATUS=""
+FULL_NOTIFIED=0
 
 while true; do
-  STATUS=$(<"$BATTERY_STATUS_PATH")
-  CAPACITY=$(<"$BATTERY_CAPACITY_PATH")
+  read -r STATUS <"$BATTERY_STATUS_PATH"
+  read -r CAPACITY <"$BATTERY_CAPACITY_PATH"
 
-  if [ "$STATUS" != "$PREV_STATUS" ] || ([ "$CAPACITY" -eq 100 ] && [ "$FULL_NOTIFIED" -eq 0 ]); then
-    MESSAGE=""
+  if [[ "$STATUS" != "$PREV_STATUS" || ("$CAPACITY" -eq 100 && FULL_NOTIFIED -eq 0) ]]; then
     case "$STATUS" in
     "Charging")
-      if [ "$CAPACITY" -eq 100 ] && [ "$FULL_NOTIFIED" -eq 0 ]; then
-        MESSAGE="🔋 Battery Full: Please unplug the charger."
+      if [[ "$CAPACITY" -eq 100 && FULL_NOTIFIED -eq 0 ]]; then
+        notify-send -t 3000 "🔋 Unplug charger."
+        paplay /usr/share/sounds/freedesktop/stereo/message-new-instant.oga
         FULL_NOTIFIED=1
-      elif [ "$PREV_STATUS" != "Charging" ]; then
-        MESSAGE="🔋 Charging: The device is currently charging."
+      elif [[ "$PREV_STATUS" != "Charging" ]]; then
+        notify-send -t 3000 "⚡ Charging..."
+        paplay /usr/share/sounds/freedesktop/stereo/power-plug.oga
       fi
       ;;
     "Discharging")
-      if [ "$PREV_STATUS" != "Discharging" ]; then
-        MESSAGE="⚡ Not Connected: The charger is not plugged in."
+      if [[ "$PREV_STATUS" != "Discharging" ]]; then
+        notify-send -t 3000 "🔌 Not charging."
+        paplay /usr/share/sounds/freedesktop/stereo/power-unplug.oga
       fi
       FULL_NOTIFIED=0
       ;;
     esac
-
-    if [ -n "$MESSAGE" ]; then
-      notify-send -u normal -t 3000 "$MESSAGE"
-    fi
     PREV_STATUS="$STATUS"
   fi
 
-  if [ "$CAPACITY" -lt 100 ]; then
-    FULL_NOTIFIED=0
+  if [[ "$CAPACITY" -lt 30 && "$STATUS" == "Discharging" ]]; then
+    notify-send -t 3000 "⚠️ Low battery!"
+    paplay /usr/share/sounds/freedesktop/stereo/dialog-warning.oga
   fi
 
-  PREV_CAPACITY="$CAPACITY"
+  ((CAPACITY < 100)) && FULL_NOTIFIED=0
 
   sleep 1
 done
