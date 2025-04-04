2c2ccbe2176baeb49cb4f07f7bed84f8f3a25eb7
aZong <giongmua2004@gmail.com>
2025-04-04 16:37:11 +0700

    feat: using open-meteo when wttr error

--- scripts/.config/scripts/checking-weather.sh
+++ scripts/.config/scripts/checking-weather.sh
@@ -6,13 +6,38 @@ process_weather() {
 
 if ping -c 3 -W 1 8.8.8.8 &>/dev/null; then
   location="Sontay"
-  weather_raw=$(curl -s "wttr.in/$location?format=%t+%c")
+  lat="21.13"
+  lon="105.50"
 
-  if [[ -n "$weather_raw" ]]; then
+  weather_raw=$(curl -s --max-time 2 "wttr.in/$location?format=%t+%c")
+
+  if [[ -z "$weather_raw" || "$weather_raw" == *"Unknown"* ]]; then
+    weather_raw=$(curl -s --max-time 3 "https://api.open-meteo.com/v1/forecast?latitude=$lat&longitude=$lon&current=temperature_2m,weather_code&timezone=Asia/Ho_Chi_Minh")
+
+    if [[ -n "$weather_raw" ]]; then
+      temperature=$(echo "$weather_raw" | jq -r '.current.temperature_2m')
+      weather_code=$(echo "$weather_raw" | jq -r '.current.weather_code')
+
+      case $weather_code in
+      0) weather_icon="󰖙" ;;                      # Clear sky
+      1 | 2 | 3) weather_icon="󰖕" ;;              # Partly cloudy
+      45 | 48) weather_icon="󰖑" ;;                # Fog
+      51 | 53 | 55 | 56 | 57) weather_icon="󰖗" ;; # Drizzle
+      61 | 63 | 65 | 66 | 67) weather_icon="󰖖" ;; # Rain
+      71 | 73 | 75 | 77) weather_icon="󰖘" ;;      # Snow
+      80 | 81 | 82) weather_icon="󰖖" ;;           # Heavy rain showers
+      85 | 86) weather_icon="󰼱" ;;                # Snow showers
+      95 | 96 | 99) weather_icon="󰼳" ;;           # Thunderstorm
+      *) weather_icon="󰅘" ;;                      # Unknown
+      esac
+
+      printf "%s, %s°C %s\n" "$location" "$temperature" "$weather_icon"
+    else
+      echo "󰅘 No Weather Information (Both APIs Failed)"
+    fi
+  else
     weather=$(process_weather "$weather_raw")
     printf "%s, %s\n" "$location" "$weather" | tr -d '+'
-  else
-    echo "󰅘 No Information"
   fi
 else
   echo "󰖪 No Internet"
