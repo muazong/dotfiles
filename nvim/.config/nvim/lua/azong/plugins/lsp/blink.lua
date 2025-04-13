174d8f7b02accaa3d8df31ad9737d79069551f7d
aZong <giongmua2004@gmail.com>
2025-04-13 15:43:28 +0700

    chore: chore

--- nvim/.config/nvim/lua/azong/plugins/lsp/blink.lua
+++ nvim/.config/nvim/lua/azong/plugins/lsp/blink.lua
@@ -5,6 +5,7 @@ return {
   -- version = "*",
   dependencies = {
     "L3MON4D3/LuaSnip",
+    build = "make install_jsregexp",
     dependencies = { "rafamadriz/friendly-snippets" },
     config = function()
       require("luasnip.loaders.from_vscode").lazy_load()
