ba5f025cbd304b5deacb3e2516a028061b46fb02
aZong <giongmua2004@gmail.com>
2025-04-26 17:05:18 +0700

    fix: update

--- nvim/.config/nvim/lua/azong/plugins/program/dotnet.lua
+++ nvim/.config/nvim/lua/azong/plugins/program/dotnet.lua
@@ -14,44 +14,41 @@ return {
     },
     config = function()
       require("roslyn").setup({
-        args = {
-          "--stdio",
-          "--logLevel=Information",
-          "--extensionLogDirectory=" .. vim.fs.dirname(vim.lsp.get_log_path()),
-          "--razorSourceGenerator=" .. vim.fs.joinpath(
-            vim.fn.stdpath("data"),
-            "mason",
-            "packages",
-            "roslyn",
-            "libexec",
-            "Microsoft.CodeAnalysis.Razor.Compiler.dll"
-          ),
-          "--razorDesignTimePath=" .. vim.fs.joinpath(
-            vim.fn.stdpath("data"),
-            "mason",
-            "packages",
-            "rzls",
-            "libexec",
-            "Targets",
-            "Microsoft.NET.Sdk.Razor.DesignTime.targets"
-          ),
-        },
         config = {
+          cmd = {
+            "roslyn",
+            "--stdio",
+            "--logLevel=Information",
+            "--extensionLogDirectory=" .. vim.fs.dirname(vim.lsp.get_log_path()),
+            "--razorSourceGenerator=" .. vim.fs.joinpath(
+              vim.fn.stdpath("data"),
+              "mason",
+              "packages",
+              "roslyn",
+              "libexec",
+              "Microsoft.CodeAnalysis.Razor.Compiler.dll"
+            ),
+            "--razorDesignTimePath=" .. vim.fs.joinpath(
+              vim.fn.stdpath("data"),
+              "mason",
+              "packages",
+              "rzls",
+              "libexec",
+              "Targets",
+              "Microsoft.NET.Sdk.Razor.DesignTime.targets"
+            ),
+          },
           handlers = require("rzls.roslyn_handlers"),
-
-          -- Ensure that hover handler is set up
           ["textDocument/hover"] = function(_, params, ctx, config)
             local client = vim.lsp.get_client_by_id(ctx.client_id)
             local bufnr = ctx.bufnr
             local hover_result = client.request_sync("textDocument/hover", params, nil, bufnr)
             return hover_result and hover_result.result or nil
           end,
-
           settings = {
             ["csharp|inlay_hints"] = {
               csharp_enable_inlay_hints_for_implicit_object_creation = true,
               csharp_enable_inlay_hints_for_implicit_variable_types = true,
-              -- ...other settings
             },
             ["csharp|code_lens"] = {
               dotnet_enable_references_code_lens = true,
