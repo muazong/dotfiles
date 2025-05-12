20013518f3d46cfd7236f7669433fdb671ae3ccc
aZong <giongmua2004@gmail.com>
2025-05-12 10:31:11 +0700

    feat: shellcheck for bash

--- nvim/.config/nvim/lua/azong/plugins/lsp/linting.lua
+++ nvim/.config/nvim/lua/azong/plugins/lsp/linting.lua
@@ -9,6 +9,7 @@ return {
       typescriptreact = { "eslint_d" },
       vue = { "eslint_d" },
       html = { "tidy" },
+      bash = { "shellcheck" },
     }
 
     local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
