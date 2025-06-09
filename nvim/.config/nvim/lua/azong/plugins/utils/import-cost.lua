abf5dc76abc3f5482acd4ff2a22c52a991fc7b20
aZong <giongmua2004@gmail.com>
2025-06-09 10:31:19 +0700

    feat: file types

--- nvim/.config/nvim/lua/azong/plugins/utils/import-cost.lua
+++ nvim/.config/nvim/lua/azong/plugins/utils/import-cost.lua
@@ -1,5 +1,16 @@
 return {
   "barrett-ruth/import-cost.nvim",
   build = "sh install.sh yarn",
+  ft = {
+    "js",
+    "jsx",
+    "javascript",
+    "ts",
+    "tsx",
+    "typescript",
+    "javascriptreact",
+    "typescriptreact",
+    "vue",
+  },
   config = true,
 }
