fc34ff0363801252bf6a95b6c6e141770f487b0b
aZong <giongmua2004@gmail.com>
2025-07-16 11:11:04 +0700

    perf: remove plugins

--- nvim/.config/nvim/lua/azong/plugins/ui/indent.lua
+++ nvim/.config/nvim/lua/azong/plugins/ui/indent.lua
@@ -1,34 +1,4 @@
 return {
-  { -- Indent scope
-    "echasnovski/mini.indentscope",
-    event = { "BufReadPre", "BufNewFile" },
-    opts = {
-      symbol = "│",
-    },
-    dependencies = {
-      "lukas-reineke/indent-blankline.nvim",
-      main = "ibl",
-      opts = {
-        indent = {
-          char = "┊",
-          tab_char = "│",
-        },
-        scope = { show_start = false, show_end = false },
-        exclude = {
-          filetypes = {
-            "help",
-            "alpha",
-            "Trouble",
-            "trouble",
-            "lazy",
-            "mason",
-            "notify",
-            "toggleterm",
-          },
-        },
-      },
-    },
-  },
   { -- Auto indent
     "vidocqh/auto-indent.nvim",
     event = { "BufReadPre", "BufNewFile" },
