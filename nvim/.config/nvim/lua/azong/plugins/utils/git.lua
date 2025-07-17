d1f4495bbf44d778625a79d30d9fd04702ae2370
aZong <giongmua2004@gmail.com>
2025-07-17 16:12:41 +0700

    refactor: update

--- nvim/.config/nvim/lua/azong/plugins/utils/git.lua
+++ nvim/.config/nvim/lua/azong/plugins/utils/git.lua
@@ -6,31 +6,20 @@ return {
       add = { text = "│" },
       change = { text = "│" },
       delete = { text = "│" },
-      topdelete = { text = "│" },
       changedelete = { text = "~" },
       untracked = { text = "┆" },
     },
-    signcolumn = true,
-    numhl = false,
-    linehl = false,
-    word_diff = false,
-    watch_gitdir = {
-      follow_files = true,
-    },
-    attach_to_untracked = true,
     current_line_blame = true,
     current_line_blame_opts = {
       virt_text = true,
       virt_text_pos = "eol",
       delay = 200,
-      ignore_whitespace = false,
-      virt_text_priority = 1000,
     },
+    watch_gitdir = {
+      follow_files = true,
+    },
+    attach_to_untracked = true,
     current_line_blame_formatter = " <author>, <author_time:%H:%M %p, %Y-%m-%d> - <summary>",
-    sign_priority = 6,
-    update_debounce = 100,
-    status_formatter = nil,
-    max_file_length = 40000,
     preview_config = {
       border = "rounded",
       style = "minimal",
@@ -40,39 +29,16 @@ return {
     },
     on_attach = function(bufnr)
       local gs = package.loaded.gitsigns
-
-      local function map(mode, l, r, opts)
-        opts = opts or {}
-        opts.buffer = bufnr
-        vim.keymap.set(mode, l, r, opts)
+      local map = function(mode, key, func)
+        vim.keymap.set(mode, key, func, { buffer = bufnr, silent = true })
       end
 
-      -- Navigation
-      map("n", "<C-]>", function()
-        if vim.wo.diff then
-          return "<C-]>"
-        end
-        vim.schedule(function()
-          gs.next_hunk()
-        end)
-        return "<Ignore>"
-      end, { expr = true, noremap = true })
-
-      map("n", "<C-[>", function()
-        if vim.wo.diff then
-          return "<C-[>"
-        end
-        vim.schedule(function()
-          gs.prev_hunk()
-        end)
-        return "<Ignore>"
-      end, { expr = true, noremap = true })
-
+      map("n", "<C-]>", gs.next_hunk)
+      map("n", "<C-[>", gs.prev_hunk)
       map("n", "<leader>hp", gs.preview_hunk)
       map("n", "<leader>hb", function()
         gs.blame_line({ full = true })
       end)
-
       map("n", "<leader>tb", gs.toggle_current_line_blame)
       map("n", "<leader>hd", gs.diffthis)
       map("n", "<leader>td", gs.toggle_deleted)
