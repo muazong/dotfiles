306b9b87992a42c0d356a121baaac5fe64df2234
aZong <giongmua2004@gmail.com>
2025-07-25 14:43:07 +0700

    chore: update snacks dashboard

--- nvim/.config/nvim/lua/azong/plugins/utils/snacks.lua
+++ nvim/.config/nvim/lua/azong/plugins/utils/snacks.lua
@@ -5,8 +5,28 @@ return {
   keys = { { "<leader>su", "<cmd>lua Snacks.picker.undo()<cr>" } },
   opts = {
     scroll = { enabled = false },
-    dashboard = { enabled = false },
 
+    dashboard = {
+      enabled = true,
+      preset = {
+        keys = {
+          { icon = " ", key = "f", desc = "Find File", action = "<CMD>Telescope find_files<CR>" },
+          { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
+          { icon = " ", key = "g", desc = "Find Text", action = "<CMD>Telescope live_grep<CR>" },
+          { icon = "󰒲 ", key = "l", desc = "Lazy", action = ":Lazy", enabled = package.loaded.lazy ~= nil },
+          { icon = " ", key = "m", desc = "Mason", action = ":Mason" },
+          { icon = " ", key = "q", desc = "Quit", action = ":qa" },
+        },
+        header = [[
+ █████╗ ███████╗ ██████╗ ███╗   ██╗ ██████╗
+██╔══██╗╚══███╔╝██╔═══██╗████╗  ██║██╔════╝
+███████║  ███╔╝ ██║   ██║██╔██╗ ██║██║  ███╗
+██╔══██║ ███╔╝  ██║   ██║██║╚██╗██║██║   ██║
+██║  ██║███████╗╚██████╔╝██║ ╚████║╚██████╔╝
+╚═╝  ╚═╝╚══════╝ ╚═════╝ ╚═╝  ╚═══╝ ╚═════╝
+ ]],
+      },
+    },
     scope = { enabled = true },
     words = { enabled = true },
     input = { enabled = true },
