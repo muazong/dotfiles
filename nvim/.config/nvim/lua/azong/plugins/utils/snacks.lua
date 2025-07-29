6cc7194db7328d3fc32cb7b85cf8f5e1b9447c88
aZong <giongmua2004@gmail.com>
2025-07-29 11:20:24 +0700

    feat: add keybind for snacks dashboard

--- nvim/.config/nvim/lua/azong/plugins/utils/snacks.lua
+++ nvim/.config/nvim/lua/azong/plugins/utils/snacks.lua
@@ -13,6 +13,7 @@ return {
           { icon = " ", key = "f", desc = "Find File", action = "<CMD>Telescope find_files<CR>" },
           { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
           { icon = " ", key = "g", desc = "Find Text", action = "<CMD>Telescope live_grep<CR>" },
+          { icon = " ", key = "b", desc = "Explorer", action = "<CMD>Telescope file_browser<CR>" },
           { icon = "󰒲 ", key = "l", desc = "Lazy", action = ":Lazy", enabled = package.loaded.lazy ~= nil },
           { icon = " ", key = "m", desc = "Mason", action = ":Mason" },
           { icon = " ", key = "q", desc = "Quit", action = ":qa" },
