2fae7df82777417beda12195b9717278d428c682
aZong <giongmua2004@gmail.com>
2025-08-06 15:03:39 +0700

    chore: update

--- nvim/.config/nvim/lua/azong/core/diagnostic.lua
+++ nvim/.config/nvim/lua/azong/core/diagnostic.lua
@@ -38,31 +38,3 @@ diagnostic.config({
   update_in_insert = false,
   severity_sort = true,
 })
-
-vim.api.nvim_create_autocmd("LspAttach", {
-  callback = function(args)
-    local client = vim.lsp.get_client_by_id(args.data.client_id)
-    if not client then
-      return
-    end
-
-    local function lsp_floating_handler(result, config, focusable)
-      if not (result and result.contents) then
-        return
-      end
-      return vim.lsp.util.open_floating_preview(
-        result.contents,
-        "markdown",
-        vim.tbl_extend("force", config or {}, { border = _border, focusable = focusable })
-      )
-    end
-
-    client.handlers["textDocument/hover"] = function(_, result, _, config)
-      return lsp_floating_handler(result, config, true)
-    end
-
-    client.handlers["textDocument/signatureHelp"] = function(_, result, _, config)
-      return lsp_floating_handler(result and result.signatures and result.signatures[1], config, false)
-    end
-  end,
-})
