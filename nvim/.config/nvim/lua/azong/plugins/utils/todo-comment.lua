5b309791e6be9a4d62a7cd410b99845acb54e974
aZong <giongmua2004@gmail.com>
2025-08-14 18:10:07 +0700

    feat: Todo comment keybind

--- nvim/.config/nvim/lua/azong/plugins/utils/todo-comment.lua
+++ nvim/.config/nvim/lua/azong/plugins/utils/todo-comment.lua
@@ -1,5 +1,6 @@
 return {
   "folke/todo-comments.nvim",
+  keys = { { "<leader>D", "<cmd>TodoTrouble<cr>", desc = "Todo Comments" } },
   dependencies = { "nvim-lua/plenary.nvim" },
   opts = {},
 }
