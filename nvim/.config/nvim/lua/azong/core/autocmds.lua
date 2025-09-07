13afb1c27b6cea25650973b6071bb47016b14350
aZong <giongmua2004@gmail.com>
2025-09-07 18:05:56 +0700

    refactor: update autocmds file

--- nvim/.config/nvim/lua/azong/core/autocmds.lua
+++ nvim/.config/nvim/lua/azong/core/autocmds.lua
@@ -1,38 +1,74 @@
 -- Disable auto comment
-vim.cmd([[autocmd FileType * setlocal formatoptions-=c formatoptions-=r formatoptions-=o]])
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "*",
+  command = "setlocal formatoptions-=c formatoptions-=r formatoptions-=o",
+})
 
 -- Rebember folds
-vim.cmd([[
-  augroup remember_folds
-    autocmd!
-    autocmd BufWinLeave *.* if &ft !=# 'help' | mkview | endif
-    autocmd BufWinEnter *.* if &ft !=# 'help' | silent! loadview | endif
-  augroup END
-]])
+local remember_folds = vim.api.nvim_create_augroup("remember_folds", { clear = true })
+vim.api.nvim_create_autocmd("BufWinLeave", {
+  group = remember_folds,
+  pattern = "*.*",
+  callback = function()
+    if vim.bo.filetype ~= "help" then
+      vim.cmd("mkview")
+    end
+  end,
+})
+vim.api.nvim_create_autocmd("BufWinEnter", {
+  group = remember_folds,
+  pattern = "*.*",
+  callback = function()
+    if vim.bo.filetype ~= "help" then
+      vim.cmd("silent! loadview")
+    end
+  end,
+})
 
 -- Highlight on yank
-vim.api.nvim_create_autocmd("TextYankPost", {
+--[[ vim.api.nvim_create_autocmd("TextYankPost", {
   callback = function()
     vim.hl.on_yank({
       higroup = "IncSearch",
       timeout = 200,
     })
   end,
-})
+}) ]]
 
 -- Auto split vertical a help filetype opened
-vim.cmd([[
-  augroup split_vertical
-  autocmd!
-  autocmd BufEnter *.txt if &buftype == 'help' | wincmd L | endif
-  augroup END
-]])
+vim.api.nvim_create_autocmd("BufEnter", {
+  group = vim.api.nvim_create_augroup("split_vertical", { clear = true }),
+  pattern = "*txt",
+  callback = function()
+    if vim.bo.buftype == "help" then
+      vim.cmd("wincmd L")
+    end
+  end,
+})
 
--- Set filetype
-vim.cmd([[autocmd BufNewFile,BufRead *.handlebars set filetype=html]])
-vim.cmd([[autocmd BufNewFile,BufRead *.hbs set filetype=html]])
-vim.cmd([[autocmd BufNewFile,BufRead *.http set filetype=http]])
-vim.cmd([[autocmd BufNewFile,BufRead *.env.* set filetype=sh]])
+-- Set custom filetypes
+local set_filetypes = vim.api.nvim_create_augroup("set_filetypes", { clear = true })
+vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
+  group = set_filetypes,
+  pattern = { "*.handlebars", "*.hbs" },
+  callback = function()
+    vim.bo.filetype = "html"
+  end,
+})
+vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
+  group = set_filetypes,
+  pattern = "*.http",
+  callback = function()
+    vim.bo.filetype = "http"
+  end,
+})
+vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
+  group = set_filetypes,
+  pattern = ".env.*",
+  callback = function()
+    vim.bo.filetype = "sh"
+  end,
+})
 
 -- Auto remove whitespace at end of line
 vim.api.nvim_create_autocmd("BufWritePre", {
