4fcf378b9ee8dd4f3a7b0cb43c06ef03c5fe11d5
aZong <giongmua2004@gmail.com>
2025-09-12 12:57:18 +0700

    chore: update

--- scripts/.config/scripts/get-keyboard.sh
+++ scripts/.config/scripts/get-keyboard.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
-
 layout=$(fcitx5-remote -n)
 
 case "$layout" in
-"keyboard-us")
-  echo "EN"
-  ;;
-"unikey")
-  echo "VI"
-  ;;
-*)
-  echo "$layout"
-  ;;
+"keyboard-us") echo "EN" ;;
+"unikey") echo "VI" ;;
+*) echo "$layout" ;;
 esac
+
+pkill -RTMIN+1 waybar 2>/dev/null
