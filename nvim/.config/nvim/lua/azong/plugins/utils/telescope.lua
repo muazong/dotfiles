6adb1d4e2ef23b9efbf23a271926ae5550f866db
aZong <giongmua2004@gmail.com>
2025-09-15 22:23:37 +0700

    chore: update

--- nvim/.config/nvim/lua/azong/plugins/utils/telescope.lua
+++ nvim/.config/nvim/lua/azong/plugins/utils/telescope.lua
@@ -3,7 +3,10 @@ return {
     "nvim-telescope/telescope.nvim",
     dependencies = {
       "nvim-lua/plenary.nvim",
-      { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+      {
+        "nvim-telescope/telescope-fzf-native.nvim",
+        build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release -DCMAKE_POLICY_VERSION_MINIMUM=3.5 && cmake --build build --config=Release && cmake --install build",
+      },
     },
     cmd = { "Telescope" },
     keys = {
