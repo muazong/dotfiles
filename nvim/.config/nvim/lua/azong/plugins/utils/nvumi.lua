77f1e62fee46f658db9004bc1ae445e2d46ec525
aZong <giongmua2004@gmail.com>
2025-09-26 09:35:23 +0700

    feat(plugin): nvumi.nvim

---
+++ nvim/.config/nvim/lua/azong/plugins/utils/nvumi.lua
@@ -0,0 +1,16 @@
+return {
+  "josephburgess/nvumi",
+  dependencies = { "folke/snacks.nvim" },
+  keys = { { "<leader>on", "<cmd>Nvumi<cr>", desc = "Open Nvumi" } },
+  opts = {
+    virtual_text = "inline", -- or "inline"
+    prefix = " 🚀 ", -- prefix shown before the output
+    date_format = "iso", -- or: "uk", "us", "long"
+    keys = {
+      run = "<CR>", -- run/refresh calculations
+      reset = "R", -- reset buffer
+      yank = "<leader>y", -- yank output of current line
+      yank_all = "<leader>Y", -- yank all outputs
+    },
+  },
+}
