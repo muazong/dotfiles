af1acd339760747996b9f2bf686b81213c9e1e09
aZong <giongmua2004@gmail.com>
2025-10-09 10:30:21 +0700

    chore: update

--- nvim/.config/nvim/lua/azong/plugins/ai/avante.lua
+++ nvim/.config/nvim/lua/azong/plugins/ai/avante.lua
@@ -1,7 +1,7 @@
 return {
   "yetone/avante.nvim",
   build = "make",
-  cmd = "AvanteToggle",
+  keys = { { "<leader>A", "<cmd>AvanteToggle<cr>", desc = "Avante Toggle" } },
   dependencies = {
     "nvim-lua/plenary.nvim",
     "MunifTanjim/nui.nvim",
@@ -9,7 +9,6 @@ return {
   config = function()
     local map = vim.keymap.set
 
-    map("n", "<leader>A", "<cmd>AvanteToggle<cr>", { desc = "Avante Toggle" })
     map("v", "<leader>aa", "<cmd>AvanteAsk<cr>", { desc = "Avante Ask" })
     map("v", "<leader>ae", "<cmd>AvanteEdit<cr>", { desc = "Avante Edit" })
 
@@ -18,7 +17,7 @@ return {
       providers = {
         gemini = {
           endpoint = "https://generativelanguage.googleapis.com/v1beta/models",
-          model = "gemini-2.5-flash-lite",
+          model = "gemini-2.5-pro",
           timeout = 30000,
           context_window = 1048576,
           use_ReAct_prompt = true,
