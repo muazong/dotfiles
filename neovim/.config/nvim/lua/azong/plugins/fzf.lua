b8059374d49208447203a4b8078d81d3e42c0eeb
aZong <giongmua2004@gmail.com>
2026-04-26 18:28:35 +0700

    feat: setup for preview image

--- neovim/.config/nvim/lua/azong/plugins/fzf.lua
+++ neovim/.config/nvim/lua/azong/plugins/fzf.lua
@@ -1,23 +1,40 @@
 vim.pack.add({
-  "https://github.com/ibhagwan/fzf-lua",
+	"https://github.com/ibhagwan/fzf-lua",
 })
 
 local function fzf_lazy(cmd)
-  vim.cmd("packadd fzf-lua")
+	vim.cmd("packadd fzf-lua")
 
-  require("fzf-lua").setup({
-    ui_select = true,
-  })
+	require("fzf-lua").setup({
+		ui_select = true,
+		previewers = {
+			builtin = {
+				extensions = {
+					["png"] = { "chafa", "{file}" },
+					["jpg"] = { "chafa", "{file}" },
+					["jpeg"] = { "chafa", "{file}" },
+					["gif"] = { "chafa", "{file}" },
+					["webp"] = { "chafa", "{file}" },
+				},
+			},
+		},
+		files = {
+			fd_opts = "--color=never --type f --hidden --follow --exclude .git --exclude node_modules",
+		},
+		grep = {
+			rg_opts = "--column --line-number --no-heading --color=always --smart-case --max-columns=4096 -e --glob '!node_modules'",
+		},
+	})
 
-  vim.keymap.set("n", "<c-f>", "<cmd>FzfLua files<cr>")
-  vim.keymap.set("n", "<c-g>", "<cmd>FzfLua live_grep<cr>")
+	vim.keymap.set("n", "<c-f>", "<cmd>FzfLua files<cr>")
+	vim.keymap.set("n", "<c-g>", "<cmd>FzfLua live_grep<cr>")
 
-  require("fzf-lua")[cmd]()
+	require("fzf-lua")[cmd]()
 end
 
 vim.keymap.set("n", "<c-f>", function()
-  fzf_lazy("files")
+	fzf_lazy("files")
 end)
 vim.keymap.set("n", "<c-g>", function()
-  fzf_lazy("live_grep")
+	fzf_lazy("live_grep")
 end)
