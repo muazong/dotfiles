da865ce66ff7e9a1fe864d3e77490d6acdd65fb1
aZong <giongmua2004@gmail.com>
2026-04-27 23:46:30 +0700

    feat: enforce minimum Neovim version
    Requires Neovim 0.12 or higher for core functionality.

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -1,3 +1,7 @@
-require("azong.core")
-require("azong.utils")
-require("azong.plugins")
+if vim.fn.has("nvim-0.12") == 1 then
+	require("azong.core")
+	require("azong.utils")
+	require("azong.plugins")
+else
+	vim.notify("Neovim version 0.12 or higher is required.", vim.log.levels.ERROR)
+end
